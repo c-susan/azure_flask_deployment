47da4c641651971880a4b8f089857cf0004c0ec1
c-susan <susan.chen@stonybrook.edu>
2023-09-12 01:20:00 +0000

    updates

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template('base.html')
 
+@app.route('/about')
+def aboutpage(): 
+    return render_template('about.html')
+
 if __name__ == '__main__':
     app.run(
         debug=True,
